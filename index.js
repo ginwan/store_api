2a3a72c6f0b6eda5a62d37ec2a9ddc746a37e51d
ginwan <ginwan697@gmail.com>
2022-08-20 21:04:34 +0200

    add routes and models

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 const app = express();
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("db connection successful"))
+  .catch((error) => console.log(error));
+
+// to accept json file
+app.use(express.json());
+// routes
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(5000, () => {
   console.log("Server is running...");
